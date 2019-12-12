bac5dcec77481bae05dfc23fb14593b81908f306
Anayis <yairalex1999@gmail.com>
2019-12-12 00:56:03 -0600

    hello

--- resources/views/layouts/navbarNoLog.blade.php
+++ resources/views/layouts/navbarNoLog.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-green bg-green">
     <a class="navbar-brand">Consultorio</a>
     <div class="navbar-nav ml-auto">
       @yield('buttonNavbar')
