86f0612dffec599eea90062799d09c5bad6d0e0f
Angel Santander <angelsantander609@gmail.com>
2019-12-11 19:53:02 -0600

    Ahora las tablas estan separadas en componentes rehutilizables

---
+++ resources/views/app/componentes/tablaHabitualPlan.blade.php
@@ -0,0 +1,31 @@
+<table class="table">
+  <thead>
+    <tr>
+      <th>Alimento</th>
+      <th>Dieta habitual</th>
+      <th>Plan de alimentación</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>Verduras</th>
+      <td><input type="number" name="habitual_verduras"></td>
+      <td>CalculoDeTabla</td>
+    </tr>
+    <tr>
+      <th>Frutas</th>
+      <td><input type="number" name="habitual_frutas"></td>
+      <td>CalculoDeTabla</td>
+    </tr>
+    <tr>
+      <th>AOA</th>
+      <td><input type="number" name="habitual_aoa"></td>
+      <td>CalculoDeTabla</td>
+    </tr>
+    <tr>
+      <th>Cerales</th>
+      <td><input type="number" name="habitual_cereales"></td>
+      <td>CalculoDeTabla</td>
+    </tr>
+  </tbody>
+</table>
