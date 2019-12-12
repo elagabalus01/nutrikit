d059270995fac3e8e705b34e4cf5018515b990f0
elagabalus <angelsantander609@gmail.com>
2019-12-12 12:16:58 -0600

    navbar

--- public/ui.js
+++ public/ui.js
@@ -1,7 +1,4 @@
-console.log("WWorking");
 $(".navbar .nav-link").on("click", function(){
-    console.log("WWorking");
-    console.log(this);
     $(".nav").find(".active").removeClass("active");
     $(this).addClass("active");
 });
