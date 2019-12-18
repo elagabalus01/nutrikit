ded6693e8314cfc835b5506a97bab363e5d3497e
Angel Santander <angelsantander609@gmail.com>
2019-12-18 15:56:43 -0600

    Pantalla cuando no hay pacientes en la base de datos

--- resources/views/app/cita_nueva.blade.php
+++ resources/views/app/cita_nueva.blade.php
@@ -4,6 +4,7 @@
 @endsection
 @section('content')
 <div class="container">
+  @if(count(App\Paciente::all())>0)
   <div class="row">
     <div class="col-md-8">
       <h1>Datos del paciente y selección de fecha</h1>
@@ -21,7 +22,7 @@
   <div class="form-group row justify-content-center">
     <label class="col-md-2 col-form-label">Fecha y hora</label>
     <div class="col-md-3">
-        <input class="form-control" id="fechaHora" type="datetime-local" min="2019-12-17" value="2019-12-18T13:45:00" id="dia_hora">
+        <input class="form-control" id="fechaHora" type="datetime-local" min="{{ substr(Carbon\Carbon::now()->toAtomString(),0,16) }}" value="{{ substr(Carbon\Carbon::now()->toAtomString(),0,16) }}" id="dia_hora">
     </div>
   </div>
   <div class="row justify-content-center">
@@ -29,15 +30,35 @@
       <button type="submit" class="btn btn-primary float-right">Aceptar</button>
     </div>
   </div>
+  @else
+  <div class="row">
+    <div class="col-md-8">
+      <h1>Aún no hay pacientes</h1>
+    </div>
+  </div>
+  @endif
+
+
 </div>
 @endsection
 @section('scripts')
 <script type="text/javascript" src="{{ asset('js/nueva_cita.js') }}"></script>
 <script type="text/javascript">
-  var mydate = new Date().toLocaleString();
-  // var mydate = new Date().toUTCString();
-  console.log(mydate);
-  // var today = new Date().toJSON(); 
+  function getLocalDateTime(){
+    var d = new Date();
+    var tz = d.toString().split("GMT")[1].split(" (")[0]; // timezone, i.e. -0700
+    console.log(tz);
+    console.log(d.toJSON());
+  }
+  getLocalDateTime();
+  // var options={dateStyle:'full',hour:'2-digit'};
+  // var mydate = new Date().toLocaleString(options);
+  // mydate=mydate.split('/');
+  // mydate=mydate.join('-');
+  // mydate=mydate.split(' ');
+  // mydate=mydate.join('T');
+  // console.log(mydate);
+
   // $('#fechaHora').attr({
   //   'min':today.slice(0,19),
   //   'value':today.slice(0,19)
