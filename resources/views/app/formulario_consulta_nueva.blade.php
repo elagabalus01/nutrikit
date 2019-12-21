3e3a3b5fb6070adc526c7b6d1bb283ac409c371f
Anayis <yairalex1999@gmail.com>
2019-12-20 23:03:14 -0600

    Diseño de los campos y validación de los rangos de los campos con números

--- resources/views/app/formulario_consulta_nueva.blade.php
+++ resources/views/app/formulario_consulta_nueva.blade.php
@@ -8,69 +8,32 @@
 @section('content')
 <div class="container">
     <div class="row">
-      <div class="col">
-        <h1>Datos del paciente</h1>
-      </div>
       <div class="col align-self-end">
         <p class="float-right">Fecha:
         {{ Carbon\Carbon::now()->format('d/m/Y') }}
         </p>
       </div>
     </div>
+ 
+<!-- Datos del paciente --> 
 
     <div class="row">
-      <div class="col-md-1">
-        <label>RFC:</label>
-      </div>
-      <div class="col-md-2">
-        <input type="text" name="rfc">
+      <div class="col">
+        <h1>Datos del paciente</h1>
       </div>
     </div>
 
     <div class="row">
-      <div class="col-md-1">
-        <label>Nombre:</label>
+      <div class="col">
+        <label>RFC:</label>
+        <input type="text" name="rfc">
       </div>
-      <div class="col-md-2">
+      <div class="col">
+        <label>Nombre:</label>
         <input class="inputNombre" type="text" name="nombre">
       </div>
     </div>
 
-   <div class="row">
-        <div class="col-md-1">
-          <label>Teléfono:</label>
-        </div>
-        <div class="col-md-2">
-          <input type="tel" name="telefono">
-      </div>
-    </div>
-    <div class="row">
-        <div class="col-md-1">
-          <label>Correo:</label>
-        </div>
-        <div class="col-md-2">
-          <input type="email" name="correo">
-       </div>
-    </div>
-
-    <br>
-    </br>
-    <div class="row">
-      <div class="col">
-        <div class="row">
-          <div class="col-md-2">
-            <label>Alergias:</label>
-          </div>
-          <div class="col-md-2">
-            <input type="text" name="alergias">
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <label>Actividad física:</label>
-        <input type="text" name="actividad_fisica_actual">
-      </div>
-    </div>
     <div class="row">
       <div class="col">
         <label>Sexo:</label>
@@ -81,69 +44,90 @@
         </select>
       </div>
       <div class="col">
+        <label>Teléfono:</label>
+        <input type="tel" name="telefono">
+      </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+          <label>Correo:</label>
+          <input type="email" name="correo">
+        </div>
+        <div class="col">
+          <label>Fecha de nacimiento:</label>
+          <input class="form-control" type="date" value="1964-12-04" id="fecha_nacimiento">
+       </div>
+    </div>
+
+    <div class="form-group row">
+        <div class="col">
+          <label>Edad: EDAD_CALCULADO</label>
+        </div>
+        <div class="col">
+          <label>IMC: IMC_CALCULADO</label>
+        </div>
+    </div>
+
+    <!-- Características del paciente --> 
+    
+    <div class="row">
+      <div class="col">
+        <h1>Características del paciente</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
         <label>Peso:</label>
-        <input class="inputEdad" type="number" name="peso_actual">
+        <input min='0.5' max='500' class="inputEdad" type="number" name="peso_actual">
         <label>Kg</label>
       </div>
       <div class="col">
         <label>Talla:</label>
-        <input class="inputEdad" type="number" name="estatura_actual">
+        <input min='1' max='255' class="inputEdad" type="number" name="estatura_actual">
         <label>cm</label>
       </div>
     </div>
-    <!-- A partir de aquí agregamos los campos nuevos  --> 
+
     <div class="row">
-        <div class="col-md-1">
+      <div class="col">
+        <label>Actividad física:</label>
+        <input type="text" name="actividad_fisica_actual">
+      </div>
+      <div class="col">
+        <label>Alergias:</label>
+        <input type="text" name="alergias">
+      </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
           <label>Porcentaje de grasa:</label>
-        </div>
-        <div class="col-md-2">
-          <input type="number" name="grasa_porcentaje">
+          <input min='0' max='100' type="number" name="grasa_porcentaje">
           <label>%</label>
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-1">
+        <div class="col">
           <label>Porcentaje de músculo:</label>
-        </div>
-        <div class="col-md-2">
-          <input type="number" name="musculo_porcentaje">
+          <input min='0' max='100' type="number" name="musculo_porcentaje">
           <label>%</label>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-md-1">
+        <div class="col">
           <label>Hueso:</label>
-        </div>
-        <div class="col-md-2">
-          <input type="number" name="hueso_kilos">
+          <input min='1' max='100' type="number" name="hueso_kilos">
           <label>kg</label>
+        </div>
+        <div class="col">
+          <label>Agua:</label>
+          <input min='1' max='100' type="number" name="agua_litros">
+          <label>L</label>
         </div>
     </div>    
 
-    <div class="row">
-        <div class="col-md-1">
-          <label>Agua:</label>
-        </div>
-        <div class="col-md-2">
-          <input type="number" name="agua_litros">
-          <label>L</label>
-        </div>
-    </div>
- <!-- Aquí termian los campos nuevos  --> 
-    <div class="form-group row">
-      <div class="col-md-3">
-        <label>Fecha de nacimiento:</label>
-        <input class="form-control" type="date" value="1964-12-04" id="fecha_nacimiento">
-      </div>
-      <div class="col">
-        <label>IMC: IMC_CALCULADO</label>
-      </div>
-      <div class="col">
-        <label>Edad: EDAD_CALCULADO</label>
-      </div>
-    </div>
+ <!--Empiezan datos de consulta --> 
 
     <div class="row">
       <div class="col">
